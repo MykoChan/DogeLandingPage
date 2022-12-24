884a702c7176c66aee73d02d631bada2c4078099
Michael Chan <chan.myko@gmail.com>
2022-12-24 13:48:34 -0500

    linted etch-a-sketch.js

--- Etch-A-Sketch/etch-a-sketch.js
+++ Etch-A-Sketch/etch-a-sketch.js
@@ -1,14 +1,14 @@
-const container = document.querySelector('.grid-container');
-const pencilButton = document.querySelector('#pencil');
-const eraserButton = document.querySelector('#eraser');
-const clearButton = document.querySelector('#clear');
-const colorPicker = document.querySelector('#color-picker');
-const canvasSizeText = document.querySelector('#canvas-size');
-const canvasSizeSlider = document.querySelector('#canvas-size-slider')
+const container = document.querySelector(".grid-container");
+const pencilButton = document.querySelector("#pencil");
+const eraserButton = document.querySelector("#eraser");
+const clearButton = document.querySelector("#clear");
+const colorPicker = document.querySelector("#color-picker");
+const canvasSizeText = document.querySelector("#canvas-size");
+const canvasSizeSlider = document.querySelector("#canvas-size-slider");
 
 // Default values
 const DEFAULT_COLOR = "black";
-const DEFAULT_MODE = "pencil"
+const DEFAULT_MODE = "pencil";
 const DEFAULT_SIZE = 16;
 
 let color = DEFAULT_COLOR;
@@ -16,36 +16,32 @@ let currentMode = DEFAULT_MODE;
 let canvasSize = DEFAULT_SIZE;
 let mousedown = false;
 
-// Event listeners
-colorPicker.addEventListener('input', setCurrentColor)
-pencilButton.addEventListener('click', setCurrentMode)
-eraserButton.addEventListener('click', setCurrentMode)
-clearButton.addEventListener('click', clearCanvas)
-canvasSizeSlider.addEventListener('mousemove', updateCanvasSizeText)
-canvasSizeSlider.addEventListener('click', updateCanvasSize)
+document.body.onmousedown = () => {
+    mousedown = true;
+};
+document.body.onmouseup = () => {
+    mousedown = false;
+};
 
-document.body.onmousedown = () => (mousedown = true);
-document.body.onmouseup = () => (mousedown = false);
+function draw(e) {
+    if (e.type === "mouseover" && !mousedown) return;
+    if (currentMode === "pencil") {
+        e.target.style.backgroundColor = color;
+    } else if (currentMode === "eraser") {
+        e.target.style.backgroundColor = "white";
+    }
+}
 
 function setupGrid(size) {
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     for (let i = 0; i < size * size; i++) {
-        let pixel = document.createElement('div');
-        pixel.classList.add('pixel');
-        pixel.addEventListener('mousedown', draw)
-        pixel.addEventListener('mouseover', draw)
+        const pixel = document.createElement("div");
+        pixel.classList.add("pixel");
+        pixel.addEventListener("mousedown", draw);
+        pixel.addEventListener("mouseover", draw);
         container.appendChild(pixel);
     }
-}
-
-function draw(e) {
-    if (e.type === 'mouseover' && !mousedown) return;
-    if (currentMode === "pencil") {
-        e.target.style.backgroundColor = color;
-    } else if (currentMode === "eraser") {
-        e.target.style.backgroundColor = "white";
-    }   
 }
 
 function setCurrentColor(e) {
@@ -60,8 +56,8 @@ function setCurrentMode(button) {
 }
 
 function clearCanvas() {
-    container.innerHTML = '';
-    setupGrid(canvasSize)
+    container.innerHTML = "";
+    setupGrid(canvasSize);
 }
 
 function updateCanvasSizeText(e) {
@@ -70,11 +66,19 @@ function updateCanvasSizeText(e) {
 
 function updateCanvasSize(e) {
     canvasSize = e.target.value;
-    updateCanvasSizeText(e)
+    updateCanvasSizeText(e);
     clearCanvas();
 }
 
 window.onload = () => {
     pencilButton.classList.add("button-selected");
     setupGrid(canvasSize);
-}
+};
+
+// Event listeners
+colorPicker.addEventListener("input", setCurrentColor);
+pencilButton.addEventListener("click", setCurrentMode);
+eraserButton.addEventListener("click", setCurrentMode);
+clearButton.addEventListener("click", clearCanvas);
+canvasSizeSlider.addEventListener("mousemove", updateCanvasSizeText);
+canvasSizeSlider.addEventListener("click", updateCanvasSize);
